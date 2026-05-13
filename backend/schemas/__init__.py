b66669f0741fdf5287f1542f886dd477548199f0
Gilbert Isaac Salazar <salazarg435@gmail.com>
2026-05-13 18:22:21 -0500

    Added calculator schemas

---
+++ backend/schemas/__init__.py
@@ -0,0 +1,9 @@
+from .users_schema import UserBase, UserRegister, UserLogin, UserResponse
+from .saved_scenario_schema import ScenarioBase, ScenarioSave, ScenarioResponse
+from .comparison_schema import ComparisonBase, ComparisonCreate, ComparisonItemResponse, ComparisonResponse
+from .calculators import (
+    TimeValueRequest, EffectiveRateRequest, ContinuousRequest,
+    AnnuityRequest, GradientRequest, LoanRequest, MaxLoanRequest,
+    RemainingBalanceRequest, BondRequest, YTMRequest,
+    SavingsRequest, InflationRequest, RealRateRequest
+)
---
+++ backend/schemas/calculators.py
@@ -0,0 +1,72 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class TimeValueRequest(BaseModel):
+    P: Optional[float] = None
+    F: Optional[float] = None
+    i: float
+    n: float
+
+class EffectiveRateRequest(BaseModel):
+    r: float
+    m: int
+
+class ContinuousRequest(BaseModel):
+    P: float
+    r: float
+    n: float
+
+class AnnuityRequest(BaseModel):
+    A: Optional[float] = None
+    P: Optional[float] = None
+    F: Optional[float] = None
+    i: float
+    n: Optional[float] = None
+
+class GradientRequest(BaseModel):
+    A: Optional[float] = None
+    G: Optional[float] = None
+    A1: Optional[float] = None
+    g = Optional[float] = None
+    i: float
+    n: float
+
+class LoanRequest(BaseModel):
+    A: float
+    i: float
+    n: float
+
+class RemainingBalanceRequest(BaseModel):
+    P: float
+    i: float
+    n: int
+    periods_paid: int
+
+class BondRequest(BaseModel):
+    F: float
+    C: Optional[float] = None
+    r: Optional[float] = None
+    i: float
+    n: int
+
+class YTMRequest(BaseModel):
+    F: float
+    C: float
+    P: float
+    n: int
+
+class SavingsRequest(BaseModel):
+    A: Optional[float] = None
+    F: Optional[float] = None
+    P: Optional[float] = None
+    i: float
+    n: Optional[float] = None
+
+class InflationRequest(BaseModel):
+    F: float
+    inflation: float
+    n: float
+
+class RealRateRequest(BaseModel):
+    i: float
+    inflation: float
